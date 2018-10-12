9ad673197e619ed6694ee73c0f926c64cbaecd26
James Noeckel <jamesn8@uw.edu>
2018-10-12 16:10:28 -0700

    some local examples, fixed projection, fov, etc

--- depth_grid_render/main.cpp
+++ depth_grid_render/main.cpp
@@ -11,14 +11,20 @@ int main(int argc, char** argv) {
         std::cout << "arg max_depth: " << max_depth << std::endl;
     }
 
-    cv::Mat depth_img = cv::imread("/Users/jamesnoeckel/Documents/C++sandbox/points_from_depth/data/tabledepth.png");
-    depth_img = 127 - depth_img;
-    cv::resize(depth_img, depth_img, cv::Size(0, 0), 0.125, 0.125);
+    //cv::Mat depth_img = cv::imread("/Users/jamesnoeckel/Documents/C++sandbox/points_from_depth/data/tabledepth.png");
+    cv::Mat depth_img = cv::imread("/Users/jamesnoeckel/Documents/C++sandbox/points_from_depth/data/maxdepth100/Depth38809_Theta165_Phi48_S.exr", cv::IMREAD_GRAYSCALE | cv::IMREAD_ANYDEPTH);
+    //cv::Mat depth_img = cv::imread("/Users/jamesnoeckel/Documents/C++sandbox/points_from_depth/data/Depth57429_Theta334_Phi47.exr", cv::IMREAD_GRAYSCALE | cv::IMREAD_ANYDEPTH);
+    depth_img = max_depth-depth_img;
+    cv::resize(depth_img, depth_img, cv::Size(0, 0), 0.25, 0.25);
     std::cout << "width: " << depth_img.cols << " height: " << depth_img.rows << std::endl;
+
+    /*cv::namedWindow("Display Window");
+    cv::imshow("Display Window", depth_img);
+    cv::waitKey(0); */
 
     double phi = 47.0 / 180 * M_PI;
     double theta = 334.0 / 180 * M_PI;
-    double fov = 90.0;
+    double fov = 38.58 / 180 * M_PI;
 
     double cx = depth_img.cols / 2.0;
     double cy = depth_img.rows / 2.0;
@@ -32,64 +38,58 @@ int main(int argc, char** argv) {
     Eigen::Matrix<double, 3, 1> up(0, 1, 0);
     Eigen::Matrix<double, 3, 1> eye(camX, camY, camZ);
 
-    cv::Mat z;
-    cv::Mat x(depth_img.rows, depth_img.cols, CV_64FC1);
-    cv::Mat y(depth_img.rows, depth_img.cols, CV_64FC1);
-
-    depth_img.convertTo(z, CV_64FC1);
-
     double constant_x = 2 * tan(fov/2.0) / depth_img.cols;
     double constant_y = constant_x; //assume uniform pinhole model
-    double greatest_z = 0;
-    double smallest_z = 1000;
-    for (int v=0; v<z.rows; v++) {
-        for (int u=0; u<z.cols;u++) {
-            double depth = z.at<double>(v, u);
-            x.at<double>(v, u) = (u - cx) * depth * constant_x;
-            y.at<double>(v, u) = (v - cy) * depth * constant_y;
-            if (depth > greatest_z) greatest_z = depth;
-            if (depth < smallest_z) smallest_z = depth;
-        }
-    }
 
-    std::cout << "maximum depth: " << greatest_z << "; smallest depth: " << smallest_z << std::endl;
-
+    double greatest_z = std::numeric_limits<double>::min();
+    double smallest_z = std::numeric_limits<double>::max();
+    double greatest_y = std::numeric_limits<double>::min();
+    double smallest_y = std::numeric_limits<double>::max();
+    int index = 1;
     cv::Mat inds(depth_img.rows, depth_img.cols, CV_64FC1);
     int discarded = 0;
     std::ofstream of("../output_mesh.obj");
-
-    int index = 1;
-    for (int v=0; v<z.rows; v++) {
-        for (int u=0; u<z.cols;u++) {
-            if (z.at<double>(v, u) > max_depth) {
+    for (int v=0; v<depth_img.rows; v++) {
+        for (int u=0; u<depth_img.cols;u++) {
+            double depth = depth_img.at<float>(v, u);
+            if (depth >= max_depth) {
                 discarded++;
                 inds.at<double>(v, u) = -1;
                 continue;
             }
-            double px = x.at<double>(v, u);
-            double py = y.at<double>(v, u);
-            double pz = z.at<double>(v, u);
-            of << "v " << px << ' ' << py << ' ' << pz << std::endl;
+            greatest_z = std::max(greatest_z, depth);
+            smallest_z = std::min(smallest_z, depth);
+            double px = (u - cx) * depth  * constant_x;
+            double py = (v - cy) * depth  * constant_y;
+            greatest_y = std::max(greatest_y, py);
+            smallest_y = std::min(smallest_y, py);
+            of << "v " << -px << ' ' << -py << ' ' << depth << std::endl;
             inds.at<double>(v, u) = index;
             index++;
         }
     }
-    std::cout << "processing " << z.cols*z.rows - discarded << " points (discarded " << discarded << ")" << std::endl;
+
+    std::cout << "maximum depth: " << greatest_z << "; smallest depth: " << smallest_z << std::endl;
+    std::cout << "maximum y: " << greatest_y << "; smallest y: " << smallest_y << std::endl;
+
+    std::cout << "processing " << depth_img.cols*depth_img.rows - discarded << " points (discarded " << discarded << ")" << std::endl;
 
 
-    for (int v=0; v<z.rows-1; v++) {
-        for (int u=0; u<z.cols-1;u++) {
-            int i00 = (int) inds.at<double>(v, u);
-            int i01 = (int) inds.at<double>(v, u+1);
-            int i10 = (int) inds.at<double>(v+1, u);
-            int i11 = (int) inds.at<double>(v+1, u+1);
-            if (i00 < 0 || i01 < 0 || i10 < 0 || i11 < 0) continue;
-            of << "f " << i00 << ' ' << i01 << ' ' << i11 << std::endl;
-            of << "f " << i00 << ' ' << i11 << ' ' << i10 << std::endl;
+    for (int v=0; v<depth_img.rows-1; v++) {
+        for (int u=0; u<depth_img.cols-1;u++) {
+            int i00 = (int) round(inds.at<double>(v, u));
+            int i01 = (int) round(inds.at<double>(v, u+1));
+            int i10 = (int) round(inds.at<double>(v+1, u));
+            int i11 = (int) round(inds.at<double>(v+1, u+1));
+            if (i00 <= 0 || i01 <= 0 || i10 <= 0 || i11 <= 0) continue;
+            of << "f " << i00 << ' ' << i11 << ' ' << i01 << std::endl;
+            of << "f " << i00 << ' ' << i10 << ' ' << i11 << std::endl;
         }
     }
 
-    std::cout << "finished" << std::endl;
+    std::cout << "finished creating mesh" << std::endl;
+
+    //system("mitsuba /Users/jamesnoeckel/Documents/C++sandbox/points_from_depth/depth_grid_render/scene.xml");
 
     return 0;
 }
